3e996484f33185bc489f0ae581fa52f1fe904a8a
jomarvillano <jomarvillano@yahoo.com>
2014-02-19 00:39:04 +0800

    Initial Files
    django-admin.py startproject VillanoOblena165mp
    configured settings.py

---
+++ VillanoOblena165mp/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for VillanoOblena165mp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VillanoOblena165mp.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
