28e7d9cdcd66882ce2bceebc03212c23f2576117
jomarvillano <jomarvillano@yahoo.com>
2014-03-06 23:21:06 +0800

    Admin interface
    Initial DB schema
    Insert via admin

---
+++ records/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Category, Location, Agent, Suspect, Crime
+
+class newLocationAdmin(admin.ModelAdmin):
+	model = Location
+
+class newSuspectAdmin(admin.ModelAdmin):
+	model = Suspect
+
+class newAgentAdmin(admin.ModelAdmin):
+	model = Agent
+
+class newCategoryAdmin(admin.ModelAdmin):
+	model = Category
+
+class newCrimeAdmin(admin.ModelAdmin):
+	model = Crime
+
+admin.site.register(Location, newLocationAdmin)
+admin.site.register(Suspect, newSuspectAdmin)
+admin.site.register(Agent, newAgentAdmin)
+admin.site.register(Category ,newCategoryAdmin)
+admin.site.register(Crime, newCrimeAdmin)
---
+++ records/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils.encoding import smart_unicode
+
+class Category(models.Model):
+	name = models.CharField(max_length = 100)
+	info = models.TextField(blank=True, default = None)
+	def __unicode__(self):
+		return self.name
+
+class Location(models.Model):
+	name = models.CharField(max_length = 100)
+	def __unicode__(self):
+		return self.name	
+
+class Agent(models.Model):
+	name = models.CharField(max_length = 100)
+	place = models.ForeignKey(Location)
+	def __unicode__(self):
+		return self.name
+
+class Suspect(models.Model):
+	name = models.CharField(max_length = 100)
+	place = models.ForeignKey(Location)
+	def __unicode__(self):
+		return self.name
+
+class Crime(models.Model):
+	classification = models.ForeignKey(Category, null=True, blank=True, default = None)
+	time = models.DateTimeField('time and date')
+	place = models.ForeignKey(Location)
+	
+	officer = models.ManyToManyField(Agent, null=True, blank=True, default = None)
+	criminal = models.ForeignKey(Suspect, null=True, blank=True, default = None)	
+	def __unicode__(self):
+		return smart_unicode(self.id)
